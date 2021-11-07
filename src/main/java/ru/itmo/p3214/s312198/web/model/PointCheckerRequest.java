ec8ceb9c1d712c3b4616154252cfe133c504206d
Yars2021 <yaroslav.sukhovey@yandex.ru>
2021-11-07 14:46:01 +0300

    Added JSP page header

--- src/main/java/ru/itmo/p3214/s312198/web/model/PointCheckerRequest.java
+++ src/main/java/ru/itmo/p3214/s312198/web/model/PointCheckerRequest.java
@@ -1,2 +1,40 @@
-package ru.itmo.p3214.s312198.web.model;public class PointCheckerRequest {
+package ru.itmo.p3214.s312198.web.model;
+
+public class PointCheckerRequest {
+    private Integer x;
+    private Float y;
+    private Integer r;
+
+    public PointCheckerRequest() {
+    }
+
+    public PointCheckerRequest(Integer x, Float y, Integer r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public void setX(Integer x) {
+        this.x = x;
+    }
+
+    public Float getY() {
+        return y;
+    }
+
+    public void setY(Float y) {
+        this.y = y;
+    }
+
+    public Integer getR() {
+        return r;
+    }
+
+    public void setR(Integer r) {
+        this.r = r;
+    }
 }
--- src/main/java/ru/itmo/p3214/s312198/web/model/PointCheckerResponse.java
+++ src/main/java/ru/itmo/p3214/s312198/web/model/PointCheckerResponse.java
@@ -1,2 +1,64 @@
-package ru.itmo.p3214.s312198.web.model;public class PointCheckerResponse {
+package ru.itmo.p3214.s312198.web.model;
+
+public class PointCheckerResponse {
+    private Integer x;
+    private Float y;
+    private Integer r;
+    private Integer exitCode;
+    private String exitMsg;
+
+    public PointCheckerResponse() {
+    }
+
+    public PointCheckerResponse(Integer x, Float y, Integer r, Integer exitCode, String exitMsg) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.exitCode = exitCode;
+        this.exitMsg = exitMsg;
+    }
+
+    public PointCheckerResponse(PointCheckerRequest request) {
+        this(request.getX(), request.getY(), request.getR(), 0, "");
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public void setX(Integer x) {
+        this.x = x;
+    }
+
+    public Float getY() {
+        return y;
+    }
+
+    public void setY(Float y) {
+        this.y = y;
+    }
+
+    public Integer getR() {
+        return r;
+    }
+
+    public void setR(Integer r) {
+        this.r = r;
+    }
+
+    public Integer getExitCode() {
+        return exitCode;
+    }
+
+    public void setExitCode(Integer exitCode) {
+        this.exitCode = exitCode;
+    }
+
+    public String getExitMsg() {
+        return exitMsg;
+    }
+
+    public void setExitMsg(String exitMsg) {
+        this.exitMsg = exitMsg;
+    }
 }
